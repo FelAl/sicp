5b05ba829b25128bdb313d2d08145d0525476ccf
af <jackoololoo@gmail.com>
2016-12-02 03:06:48 +0300

    1.6 solved

--- book/1/1.6.scm
+++ book/1/1.6.scm
@@ -48,3 +48,12 @@
 (sqrt_nf 9)
 ;Aborting!: maximum recursion depth exceeded
 ; additional level of recursion
+; =========
+; (new-if (good-enough? guess) 
+; guess
+; (sqrt-iter (improve guess)))
+; In our new-if procedure , two branches execute:
+;   ( if true  then guess or if true then (sqrt-iter ...))
+; In `classical` if only one branch executes
+; So, in our example, we have a kind of `recursion overflow`
+; new-if calls sqrt-iter, sqrt-iter calls new new-if and there is no end to this
